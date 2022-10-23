7cc3d90597de1e55eb4a97e4061f2b9e758fc05b
Ando Deng <87212291+AD1938@users.noreply.github.com>
2022-10-23 00:46:18 -0400

    done

---
+++ test.py
@@ -0,0 +1,11 @@
+import time
+import pyautogui
+pyautogui.moveTo(640, 900)
+pyautogui.click()
+while True:
+  time.sleep(5)
+  pyautogui.write("gsgsnnn")
+  time.sleep(5)
+
+  pyautogui.press("enter")
+
---
+++ type.py
@@ -0,0 +1,9 @@
+import pyautogui
+pyautogui.moveTo(1000,200)
+pyautogui.click()
+while True:
+
+  pyautogui.write("much never another line in through need interest might end")
+  pyautogui.press('tab')
+  pyautogui.press('tab')
+  pyautogui.press('enter')
